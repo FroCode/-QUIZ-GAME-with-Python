a5a9844c89690f5aa5f7c5c8d1d8f0b0285cb4fd
Haddouche Maamar <frocodeof@gmail.com>
2023-10-19 23:39:18 +0200

    Add Questions

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-question = ("What is your name? ")
-print(question)
+question = (" How many elements are in the periodic table?",
+               "Which anilmal is the largest eggs ?",
+                 "What is the most abundant gas in Earth's atmosphere: ? ",
+                   "How Many Bones are in the human body: ?",
+                     "Which PLanet int the solar system is the hottest: ?")
+
+options = ((), (), (), ())
+answers = ()
+gueses = []
+score = 0
+question_number = 0
