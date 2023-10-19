c51ef7343187d45fe78accaa8babaf0cbeaf523f
Haddouche Maamar <frocodeof@gmail.com>
2023-10-19 23:42:57 +0200

    Add Answers

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-question = (" How many elements are in the periodic table?",
+questions = (" How many elements are in the periodic table?",
                "Which anilmal is the largest eggs ?",
                  "What is the most abundant gas in Earth's atmosphere: ? ",
                    "How Many Bones are in the human body: ?",
                      "Which PLanet int the solar system is the hottest: ?")
 
-options = ((), (), (), ())
+options = (("A.", "B." , "C" , "D."), ("A" "B.", "C." , "D"), ("A." ,"B." ,"C.", "D"), ("A", "B", "C" "D."))
 answers = ()
 gueses = []
 score = 0
-question_number = 0
